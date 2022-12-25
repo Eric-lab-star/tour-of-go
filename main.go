c9c4676334d6a166da7057609c9dc529e0668ee5
eric-lab-star <cyon256@icloud.com>
2022-12-25 19:22:34 +0900

    os.Getwd() with os.DirFS()

--- main.go
+++ main.go
@@ -6,6 +6,17 @@ import (
 )
 
 func main() {
-	fs := os.DirFS("/main.go")
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	fs := os.DirFS(wd)
 	fmt.Printf("type %T, value %v\n", fs, fs)
+	file, err := fs.Open("main.go")
+	if err != nil {
+		panic(err)
+	}
+	data := make([]byte, 100)
+	file.Read(data)
+	fmt.Println(string(data))
 }
