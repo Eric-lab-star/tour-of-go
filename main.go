1ae7324d9c1f28196c9a0c39ce504c66608b7658
eric-lab-star <cyon256@icloud.com>
2022-12-27 21:04:36 +0900

    main reset

--- main.go
+++ main.go
@@ -3,13 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	primes := [4]int{2, 3, 5, 7}
 
-	slice := primes[0:2]
-	// slice2 := primes[0:4]
-	// slice2[3] = 11
-	slice = append(slice, 11, 13)
-	slice[3] = 17
-	fmt.Println(primes)
+	fmt.Println("hello world")
 
 }
