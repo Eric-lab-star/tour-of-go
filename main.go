34c33a131b6b7423a59d62ac6e172063bed55189
eric-lab-star <cyon256@icloud.com>
2022-12-28 16:21:13 +0900

    map

--- main.go
+++ main.go
@@ -2,8 +2,18 @@ package main
 
 import "fmt"
 
-func main() {
+type student struct {
+	first_name, last_name string
+}
 
-	fmt.Println("hello world")
+var m map[string]student
+
+func main() {
+	m = make(map[string]student)
+	m["korean"] = student{
+		first_name: "Kyungsub",
+		last_name:  "Kim",
+	}
+	fmt.Println(m["korean"])
 
 }
