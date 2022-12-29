e4c3d62cc12ac692a1300d7892f3e6677da967c7
eric-lab-star <cyon256@icloud.com>
2022-12-29 11:31:06 +0900

    empty interface

--- main.go
+++ main.go
@@ -2,28 +2,12 @@ package main
 
 import "fmt"
 
-type Vertex struct {
-	X, Y float64
-}
-
-func (v Vertex) add() float64 {
-	return v.X + v.Y
-}
-
-func (v *Vertex) scale(f float64) {
-	v.X = v.X * f
-	v.Y = v.Y * f
-}
-
-func mutate(v *Vertex) {
-	v.X = v.X * 2
-	v.Y = v.Y * 2
-}
-
 func main() {
-	v := Vertex{3, 2}
-	fmt.Println(v)
-	mutate(&v)
-	fmt.Println(v)
+	var i interface{}
+
+	i = 10
+	i = "hello"
+
+	fmt.Println(i)
 
 }
