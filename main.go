7c126bcc3c9689c401a218e1291d305c0b237f6d
eric-lab-star <cyon256@icloud.com>
2022-12-30 21:52:05 +0900

    goroutine

--- main.go
+++ main.go
@@ -2,25 +2,38 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func write(ch chan int) {
-	for i := 0; i < 5; i++ {
-		ch <- i
-		fmt.Printf("successfully wrote %d\n", i)
-	}
-	close(ch)
+// SafeCounter is safe to use concurrently.
+type SafeCounter struct {
+	mu sync.Mutex
+	v  map[string]int
+}
 
+// Inc increments the counter for the given key.
+func (c *SafeCounter) Inc(key string) {
+	c.mu.Lock()
+	// Lock so only one goroutine at a time can access the map c.v.
+	c.v[key]++
+	c.mu.Unlock()
+}
+
+// Value returns the current value of the counter for the given key.
+func (c *SafeCounter) Value(key string) int {
+	c.mu.Lock()
+	// Lock so only one goroutine at a time can access the map c.v.
+	defer c.mu.Unlock()
+	return c.v[key]
 }
 
 func main() {
-	ch := make(chan int, 2)
-
-	go write(ch)
-	time.Sleep(2 * time.Second)
-	for v := range ch {
-		fmt.Printf("read value %v\n", v)
-		time.Sleep(1 * time.Second)
+	c := SafeCounter{v: make(map[string]int)}
+	for i := 0; i < 1000; i++ {
+		go c.Inc("somekey")
 	}
+
+	time.Sleep(time.Second)
+	fmt.Println(c.Value("somekey"))
 }
